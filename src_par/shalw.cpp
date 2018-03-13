3204f263aa84bd9177e62b0e9eaf847374991ef7
ClementChouteau <clement.chouteau@gmail.com>
2018-03-13 14:01:17 +0100

    Fin ajouts du précédent commit.

---
+++ src_par/shalw.cpp
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <shalw.h>
+#include <parse_args.hpp>
+#include <memory.h>
+#include <init.h>
+#include <forward.h>
+#include <export.h>
+#include <mpi.h>
+
+#undef DEBUG
+
+#ifdef DEBUG
+#define PRINT(X) printf(x)
+#else
+#define PRINT(X)
+#endif
+
+double *hFil, *uFil, *vFil, *hPhy, *uPhy, *vPhy;
+int size_x, size_y, nb_steps;
+double dx, dy, dt, pcor, grav, dissip, hmoy, alpha, height, epsilon;
+bool file_export;
+std::string export_path;
+int p, id;
+bool async;
+
+int main(int argc, char **argv) {
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+	parse_args(argc, argv);
+	PRINT("Command line options parsed\n");
+
+	if (size_y % p != 0) {
+		printf("Height of image not divisible by number of workers\n");
+		exit(1);
+	}
+
+	// 1 seul export fait par root
+	if (p > 1 && id != 0)
+		file_export = false;
+
+	alloc();
+	PRINT("Memory allocated\n");
+
+	gauss_init();
+	PRINT("State initialised\n");
+
+	if (async)
+		forward_async();
+	else
+		forward_sync();
+	PRINT("State computed\n");
+
+	dealloc();
+	PRINT("Memory freed\n");
+
+	MPI_Finalize();
+
+	return EXIT_SUCCESS;
+}
