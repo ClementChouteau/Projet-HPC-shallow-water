cc62803274bbb549ca8386c731a3e7c46d18af4e
Romain Mekarni <romain.mekarni@gmail.com>
2018-04-02 13:33:30 +0200

    async working but block very slow

--- parallel/include/export.h
+++ parallel/include/export.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 void create_file(int);
-void export_step_bands_sync(int);
-void export_step_blocks_sync(int t);
+void export_step_bands(int, int);
+void export_step_blocks(int, int);
 void finalize_export(void);
