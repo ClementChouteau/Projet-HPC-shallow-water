398a1d0a99be2978b7e82b4bf30c6066e9b6cf74
Romain Mekarni <romain.mekarni@gmail.com>
2018-04-02 14:42:11 +0200

    add times rule to makefile for testing parallel performances

--- make_hostfile.sh
+++ make_hostfile.sh
@@ -1,1 +1,5 @@
-nmap -sP 132.227.114.225/27 | grep ppti | cut -d ' ' -f 5 | cut -d '.' -f 1 | grep -vE "302-06|-gw" | sed 's/$/ slots=1/' > hostfile
+if [[ $1 == "403" ]]; then
+    nmap -sP 132.227.114.160/27 | grep ppti | cut -d ' ' -f 5 | cut -d '.' -f 1 | sed 's/$/ slots=1/' > hostfile
+else
+    echo "localhost slots=4" > hostfile
+fi
