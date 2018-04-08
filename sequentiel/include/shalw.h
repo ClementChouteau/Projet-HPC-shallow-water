e5ec986b52404905bfa90256fc91564463a4dc00
clement.chouteau@gmail.com <clement.chouteau@gmail.com>
2018-04-08 16:00:23 +0200

    Optimisation des accès mémoire.

--- sequentiel/include/shalw.h
+++ sequentiel/include/shalw.h
@@ -5,9 +5,9 @@ extern double	  dx, dy, dt, pcor, grav, dissip, hmoy, alpha, height, epsilon;
 extern bool		   file_export;
 extern std::string export_path;
 
-#define HFIL(t, i, j) hFil[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
-#define UFIL(t, i, j) uFil[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
-#define VFIL(t, i, j) vFil[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
-#define HPHY(t, i, j) hPhy[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
-#define UPHY(t, i, j) uPhy[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
-#define VPHY(t, i, j) vPhy[(j) + (i)*size_y + ((t) % 2) * size_x * size_y]
+#define HFIL(t, i, j) hFil[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
+#define UFIL(t, i, j) uFil[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
+#define VFIL(t, i, j) vFil[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
+#define HPHY(t, i, j) hPhy[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
+#define UPHY(t, i, j) uPhy[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
+#define VPHY(t, i, j) vPhy[(i) + (j)*size_x + ((t) % 2) * size_x * size_y]
